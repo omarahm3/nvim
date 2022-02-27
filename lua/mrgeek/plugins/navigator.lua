f1842e353396a286d7d524f8e33401d1dbf48a7f
Omar Ahmed <omarking05@gmail.com>
2022-02-27 17:05:43 +0200

    feat(navigator): disable virtual text and some lsp servers

--- lua/mrgeek/plugins/navigator.lua
+++ lua/mrgeek/plugins/navigator.lua
@@ -40,18 +40,18 @@ local default = {
   },
   lsp_installer = false, -- set to true if you would like use the lsp installed by williamboman/nvim-lsp-installer
   lsp = {
-    code_action = {enable = true, sign = true, sign_priority = 40, virtual_text = true},
-    code_lens_action = {enable = true, sign = true, sign_priority = 40, virtual_text = true},
-    format_on_save = true, -- set to false to disable lsp code format on save (if you are using prettier/efm/formater etc)
-    disable_format_cap = {"sqls", "sumneko_lua", "gopls"},  -- a list of lsp disable format capacity (e.g. if you using efm or vim-codeformat etc), empty {} by default
-    disable_lsp = {'pylsd', 'sqlls'}, -- a list of lsp server disabled for your project, e.g. denols and tsserver you may
+    code_action = {enable = true, sign = true, sign_priority = 40, virtual_text = false},
+    code_lens_action = {enable = true, sign = true, sign_priority = 40, virtual_text = false},
+    format_on_save = false, -- set to false to disable lsp code format on save (if you are using prettier/efm/formater etc)
+    disable_format_cap = {"sqls", "sumneko_lua"},  -- a list of lsp disable format capacity (e.g. if you using efm or vim-codeformat etc), empty {} by default
+    disable_lsp = {'pylsd', 'sqlls', 'ngserver', 'vscode-html-language-server'}, -- a list of lsp server disabled for your project, e.g. denols and tsserver you may
     -- only want to enable one lsp server
     -- to disable all default config and use your own lsp setup set
     -- disable_lsp = 'all'
     -- Default {}
     diagnostic_scrollbar_sign = {'▃', '▆', '█'}, -- experimental:  diagnostic status in scroll bar area; set to false to disable the diagnostic sign,
     -- for other style, set to {'╍', 'ﮆ'} or {'-', '='}
-    diagnostic_virtual_text = true,  -- show virtual for diagnostic message
+    diagnostic_virtual_text = false,  -- show virtual for diagnostic message
     diagnostic_update_in_insert = false, -- update diagnostic message in insert mode
     disply_diagnostic_qf = true, -- always show quickfix if there are diagnostic errors, set to false if you  want to ignore it
     tsserver = {
@@ -63,7 +63,7 @@ local default = {
         -- your special on attach here
         -- e.g. disable gopls format because a known issue https://github.com/golang/go/issues/45732
         -- print("i am a hook, I will disable document format")
-        client.resolved_capabilities.document_formatting = false
+        -- client.resolved_capabilities.document_formatting = false
       end,
       settings = {
         gopls = {gofumpt = false} -- disable gofumpt etc,
