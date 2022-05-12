98c746c2d1f4856d96e75f21605721aad3dc2946
Omar Ahmed <omarking05@gmail.com>
2022-05-12 19:47:51 +0200

    feat(lsp): loop through servers table

--- lua/mrgeek/lsp/run.lua
+++ lua/mrgeek/lsp/run.lua
@@ -12,28 +12,46 @@ local function setup_server(server_name, server_config)
   lspconfig[server_name].setup(resolve_config(server_config))
 end
 
-setup_server('eslint', {
-  on_attach = require('mrgeek.lsp.settings.eslint').on_attach,
-  settings = require('mrgeek.lsp.settings.eslint').settings,
-})
+local servers = {
+  {
+    name = 'eslint',
+    setup = {
+      on_attach = require('mrgeek.lsp.settings.eslint').on_attach,
+      settings = require('mrgeek.lsp.settings.eslint').settings,
+    }
+  },
+  {
+    name = 'jsonls',
+    setup = {
+      on_attach = require('mrgeek.lsp.settings.eslint').on_attach,
+      settings = require('mrgeek.lsp.settings.eslint').settings,
+    }
+  },
+  {
+    name = 'sumneko_lua',
+    setup = {
+      settings = require('mrgeek.lsp.settings.sumneko_lua').settings,
+    }
+  },
+  {
+    name = 'tsserver',
+    setup = {
+      capabilities = require('mrgeek.lsp.settings.tsserver').capabilities,
+      on_attach = require('mrgeek.lsp.settings.tsserver').on_attach,
+    }
+  },
+  {
+    name = 'intelephense',
+    setup = {
+      root_dir = lspconfig.util.root_pattern('composer.json'),
+      filetypes = { 'php' },
+    }
+  },
+}
 
-setup_server('jsonls', {
-  settings = require('mrgeek.lsp.settings.jsonls').settings,
-})
-
-setup_server('sumneko_lua', {
-  settings = require('mrgeek.lsp.settings.sumneko_lua').settings,
-})
-
-setup_server('tsserver', {
-  capabilities = require('mrgeek.lsp.settings.tsserver').capabilities,
-  on_attach = require('mrgeek.lsp.settings.tsserver').on_attach,
-})
-
-setup_server('intelephense', {
-  root_dir = lspconfig.util.root_pattern('composer.json'),
-  filetypes = { 'php' },
-})
+for _, server in ipairs(servers) do
+  setup_server(server.name, server.setup)
+end
 
 for _, server in ipairs { 'bashls', 'cssls', 'html', 'phpactor', 'gopls' } do
   setup_server(server, {})
