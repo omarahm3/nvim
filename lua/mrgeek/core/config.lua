2f32c96895b74085e1fb1e23bdc3cb93599f0d5e
Omar Ahmed <omarking05@gmail.com>
2022-05-13 18:10:12 +0200

    feat(config): add some keymappings to which_key

--- lua/mrgeek/core/config.lua
+++ lua/mrgeek/core/config.lua
@@ -25,7 +25,6 @@ local float = {
 MrGeek = {
   log = {
     level = 'debug',
-    override_notify = false,
   },
   icons = icons,
   theme = {
@@ -61,7 +60,6 @@ MrGeek = {
       max_width = 30,
       context = 30,
     },
-    automatic_servers_installation = true,
     buffer_mappings = {
       normal_mode = {
         ['K'] = { vim.lsp.buf.hover, 'Show hover' },
@@ -116,9 +114,88 @@ MrGeek = {
       numhl = 'LspDiagnosticsSignInformation',
     },
   },
-  which_key = {
-    mappings = {
-      d = {}
+  mappings = {
+    opts = {
+      mode = 'n', -- NORMAL mode
+      prefix = '<leader>',
+      buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+      silent = true, -- use `silent` when creating keymaps
+      noremap = true, -- use `noremap` when creating keymaps
+      nowait = true, -- use `nowait` when creating keymaps
+    },
+    vopts = {
+      mode = 'v', -- VISUAL mode
+      prefix = '<leader>',
+      buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+      silent = true, -- use `silent` when creating keymaps
+      noremap = true, -- use `noremap` when creating keymaps
+      nowait = true, -- use `nowait` when creating keymaps
+    },
+    keymappings = {
+      ['w'] = { ':w!<CR>', 'Save file' },
+      ['x'] = { ':lua require("mrgeek.utils").close_buffer()<CR>', 'Close current buffer' },
+
+      -- Buffers related
+      -- TODO Check how to register global keymappings without holding to leader
+      -- ['<ESC>'] = { ':noh<CR>', 'No highlight' },
+      -- ['jk'] = { '<ESC>', 'Escape insert mode and go to visual mode' },
+      -- ['<C-s>'] = { ':w!<CR>', 'Save file' },
+      -- ['<Tab>'] = { ':BufferLineCycleNext<CR>', 'Next' },
+      -- ['<S-Tab>'] = { ':BufferLineCyclePrev<CR>', 'Previous' },
+
+      b = {
+        name = 'Buffers',
+        j = { ':BufferLinePick<CR>', 'Jump' },
+        f = { ':Telescope buffers<CR>', 'Find' },
+        p = { ':BufferLineCyclePrev<CR>', 'Previous' },
+        n = { ':BufferLineCycleNext<CR>', 'Next' },
+        e = { ':BufferLinePickClose<CR>', 'Pick buffer to close' },
+        D = { ':BufferLineSortByDirectory<CR>', 'Sort by directory' },
+        L = { ':BufferLineSortByExtension<CR>', 'Sort by language' },
+      },
+      s = {
+        name = 'Session',
+        s = { ':SessionSave<CR>', 'Save session' },
+        l = { ':SessionLoad<CR>', 'Load session' },
+      },
+      g = {
+        name = 'Git',
+        j = { ':lua require "gitsigns".next_hunk()<CR>', 'Next hunk' },
+        k = { ':lua require "gitsigns".prev_hunk()<CR>', 'Previous hunk' },
+        l = { ':lua require "gitsigns".blame_line({full=true})<CR>', 'Blame line' },
+        B = { ':0Gclog<CR>', 'Blame file' },
+        p = { ':lua require "gitsigns".preview_hunk()<CR>', 'Preview hunk' },
+        r = { ':lua require "gitsigns".reset_hunk()<CR>', 'Reset hunk' },
+        R = { ':lua require "gitsigns".reset_buffer()<CR>', 'Reset buffer' },
+        h = { ':lua require "gitsigns".stage_hunk()<CR>', 'Stage hunk' },
+        u = { ':lua require "gitsigns".undo_stage_hunk()<CR>', 'Undo stage hunk' },
+        s = { ':G<CR>', 'Status' },
+        b = { ':Telescope git_branches<CR>', 'Checkout branch' },
+        c = { ':Telescope git_branches<CR>', 'Checkout branch' },
+        d = { ':lua require "gitsigns".diffthis("~")<CR>', 'Diff current file with remote' },
+        L = { ':lua _LAZYGIT_TOGGLE()<CR>', 'LazyGit' },
+        t = {
+          name = 'Toggle',
+          l = { ':lua require "gitsigns".toggle_current_line_blame()<CR>', 'Blame line' },
+          d = { ':lua require "gitsigns".toggle_deleted()<CR>', 'Deleted hunks' },
+        },
+        v = {
+          name = 'Diff View',
+          o = { ':DiffviewOpen -uno<CR>', 'Open' },
+          c = { ':DiffviewClose<CR>', 'Close' },
+          r = { ':DiffviewRefresh<CR>', 'Refresh' },
+          f = { ':DiffviewToggleFiles<CR>', 'Toggle files' },
+          s = { ':DiffviewFocusFiles<CR>', 'Focus files' },
+          h = { ':DiffviewFileHistory %:p<CR>', 'File history' },
+        }
+      },
+      l = {
+        name = 'LSP',
+        a = { ':lua vim.lsp.buf.code_action()<CR>', 'Code action' },
+        d = { ':Telescope diagnostics bufnr=0 theme=get_ivy<CR>', 'Buffer diagnostics' },
+        w = { ':Telescope diagnostics<CR>', 'Diagnostics' },
+        f = { ':lua require"mrgeek.lsp.functions".format()<CR>', 'Format' },
+      }
     }
   }
 }
