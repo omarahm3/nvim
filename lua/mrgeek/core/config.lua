350c70cfac4534dbfac3f496ae73b45d47e2b38b
Omar Ahmed <omarking05@gmail.com>
2022-05-14 06:47:07 +0200

    feat(config): add dap debug keymappings

--- lua/mrgeek/core/config.lua
+++ lua/mrgeek/core/config.lua
@@ -189,7 +189,11 @@ MrGeek = {
         name = 'Focus',
         c = { ':YodeCreateSeditorFloating<CR>', 'Yoda create' },
         r = { ':YodeCreateSeditorReplace<CR>', 'Yoda replace' },
-      }
+      },
+      d = {
+        name = "Debug",
+        e = { "<cmd>lua require'dapui'.eval()<cr>", "Evaluate" },
+      },
     },
     keymappings = {
       ['w'] = { ':w!<CR>', 'Save file' },
@@ -205,6 +209,29 @@ MrGeek = {
         e = { ':BufferLinePickClose<CR>', 'Pick buffer to close' },
         D = { ':BufferLineSortByDirectory<CR>', 'Sort by directory' },
         L = { ':BufferLineSortByExtension<CR>', 'Sort by language' },
+      },
+      d = {
+        name = "Debug",
+        R = { "<cmd>lua require'dap'.run_to_cursor()<cr>", "Run to Cursor" },
+        E = { "<cmd>lua require'dapui'.eval(vim.fn.input '[Expression] > ')<cr>", "Evaluate Input" },
+        C = { "<cmd>lua require'dap'.set_breakpoint(vim.fn.input '[Condition] > ')<cr>", "Conditional Breakpoint" },
+        U = { "<cmd>lua require'dapui'.toggle()<cr>", "Toggle UI" },
+        b = { "<cmd>lua require'dap'.step_back()<cr>", "Step Back" },
+        c = { "<cmd>lua require'dap'.continue()<cr>", "Continue" },
+        d = { "<cmd>lua require'dap'.disconnect()<cr>", "Disconnect" },
+        e = { "<cmd>lua require'dapui'.eval()<cr>", "Evaluate" },
+        g = { "<cmd>lua require'dap'.session()<cr>", "Get Session" },
+        h = { "<cmd>lua require'dap.ui.widgets'.hover()<cr>", "Hover Variables" },
+        S = { "<cmd>lua require'dap.ui.widgets'.scopes()<cr>", "Scopes" },
+        i = { "<cmd>lua require'dap'.step_into()<cr>", "Step Into" },
+        o = { "<cmd>lua require'dap'.step_over()<cr>", "Step Over" },
+        p = { "<cmd>lua require'dap'.pause.toggle()<cr>", "Pause" },
+        q = { "<cmd>lua require'dap'.close()<cr>", "Quit" },
+        r = { "<cmd>lua require'dap'.repl.toggle()<cr>", "Toggle Repl" },
+        s = { "<cmd>lua require'dap'.continue()<cr>", "Start" },
+        t = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Toggle Breakpoint" },
+        x = { "<cmd>lua require'dap'.terminate()<cr>", "Terminate" },
+        u = { "<cmd>lua require'dap'.step_out()<cr>", "Step Out" },
       },
       s = {
         name = 'Session',
@@ -305,7 +332,7 @@ MrGeek = {
         d = { ':YodeBufferDelete<CR>', 'Yoda delete buffer' },
         a = { ':TZAtaraxis<CR>', 'Ataraxis mode' },
         f = { ':TZFocus<CR>', 'Focus mode' },
-        m = { ':TZMinimalist<CR>', 'Minimalist mode' },
+        m = { ':TZMinimalist<CR>', 'Focus mode' },
       },
     }
   }
