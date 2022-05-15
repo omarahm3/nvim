6404fe2c7b21979c1a968c2b1d95638364d93ef3
Omar Ahmed <omarking05@gmail.com>
2022-05-15 15:24:30 +0200

    feat(core): remap leader key

--- lua/mrgeek/core/init.lua
+++ lua/mrgeek/core/init.lua
@@ -1,2 +1,14 @@
 require 'mrgeek.core.config'
 require 'mrgeek.core.keymappings'
+
+-- Remap space as a leader key
+local function remap_leader()
+  vim.api.nvim_set_keymap('', MrGeek.default_leader, '<Nop>', {
+    noremap = true,
+    silent = true,
+  })
+  vim.g.mapleader = (MrGeek.default_leader == '<Space>' and '') or MrGeek.default_leader
+  vim.g.maplocalleader =(MrGeek.default_leader == '<Space>' and '') or MrGeek.default_leader
+end
+
+remap_leader()
