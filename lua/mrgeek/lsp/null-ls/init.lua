47994a71e7d19b69df16a1465260085c0aadbfc9
Omar Ahmed <omarking05@gmail.com>
2022-07-13 10:30:07 +0200

    feat(null-ls): add write-good server

--- lua/mrgeek/lsp/null-ls/init.lua
+++ lua/mrgeek/lsp/null-ls/init.lua
@@ -96,6 +96,22 @@ function M.setup()
 
     -- Spell check
     b.diagnostics.codespell,
+    b.diagnostics.write_good.with({
+      filetypes = {
+        "javascript",
+        "javascriptreact",
+        "typescript",
+        "typescriptreact",
+        "css",
+        "scss",
+        "less",
+        "html",
+        "json",
+        "jsonc",
+        "yaml",
+        "markdown",
+      },
+    }),
 
     -- Vale
     b.diagnostics.vale,
