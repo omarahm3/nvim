484223a366fab08f31cadaccfe6a7ca28c24f581
Omar Ahmed <omarking05@gmail.com>
2022-07-15 16:14:13 +0200

    feat(plugins): use simpler plugins module

--- lua/mrgeek/plugins.lua
+++ lua/mrgeek/plugins.lua
@@ -1,55 +1,18 @@
-local fn = vim.fn
+local packer = nil
+local function init()
+  if packer == nil then
+    packer = require 'packer'
+    packer.init { disable_commands = true }
+  end
 
--- Automatically install packer
-local install_path = fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
+  local use = packer.use
+  packer.reset()
 
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system({
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  })
-  print("Installing packer close and reopen Neovim...")
-  vim.cmd([[packadd packer.nvim]])
-end
-
--- autocommand that will reload neovim whenever you save plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
-
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-  return
-end
-
-packer.init({
-  display = {
-    open_fn = function()
-      return require("packer.util").float({ border = "single" })
-    end,
-    prompt_border = "single",
-  },
-  git = {
-    clone_timeout = 6000, -- seconds
-  },
-  max_jobs = 20,
-  compile_on_sync = true,
-})
-
-return packer.startup(function(use)
-  -- Packer plugin --
   use({
     "lewis6991/impatient.nvim",
   })
 
+  -- Packer plugin --
   use({
     "wbthomason/packer.nvim", -- have packer manage itself
     event = "VimEnter",
@@ -571,11 +534,13 @@ return packer.startup(function(use)
     "edluffy/specs.nvim",
     config = [[ require("specs").setup({}) ]],
   })
+end
 
-  -- Packer stuff --
-  -- automatically set up configuration after cloning packer
-  -- this must be at the end of all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
-end)
+local plugins = setmetatable({}, {
+  __index = function(_, key)
+    init()
+    return packer[key]
+  end,
+})
+
+return plugins
