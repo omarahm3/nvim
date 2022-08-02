449e17262bfd07aad21e082e720ef34e15ee244d
Omar Ahmed <omarking05@gmail.com>
2022-08-02 14:10:48 +0200

    feat(lsp): use mason to install servers

--- lua/mrgeek/lsp/lsp-installer.lua
+++ lua/mrgeek/lsp/lsp-installer.lua
@@ -1,13 +1,34 @@
-local present, lsp_installer = pcall(require, 'nvim-lsp-installer')
+local mason_present, mason = pcall(require, "mason")
+local mason_lsp_present, mason_lsp = pcall(require, "mason-lspconfig")
 
-if not present then
-  vim.notify('nvim-lsp-installer does not exist')
+if not mason_present or not mason_lsp_present then
+  vim.notify("mason or mason_lsp does not exist")
   return
 end
 
-lsp_installer.setup {
+mason.setup({
+  ui = {
+    border = "rounded",
+  },
+})
+
+mason_lsp.setup({
   -- A list of servers to automatically install if they're not already installed
-  ensure_installed = { 'bashls', 'cssls', 'eslint', 'html', 'jsonls', 'sumneko_lua', 'tsserver', 'intelephense', 'gopls', 'pyright' },
+  ensure_installed = {
+    "bash-language-server",
+    "css-lsp",
+    "html-lsp",
+    "json-lsp",
+    "lua-language-server",
+    "eslint-lsp",
+    "tailwindcss-language-server",
+    "typescript-language-server",
+    "chrome-debug-adapter",
+    "node-debug2-adapter",
+    "intelephense",
+    "gopls",
+    "pyright",
+  },
   -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed
   automatic_installation = true,
-}
+})
