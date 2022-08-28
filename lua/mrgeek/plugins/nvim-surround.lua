3e6b7692f2c0c1e9813538e69618e94454907fc0
Omar Ahmed <omarking05@gmail.com>
2022-08-28 08:51:20 +0200

    fix(nvim-surround): keep only custom config

--- lua/mrgeek/plugins/nvim-surround.lua
+++ lua/mrgeek/plugins/nvim-surround.lua
@@ -4,8 +4,6 @@ if not present then
   return
 end
 
-local utils = require("nvim-surround.utils")
-
 local default = {
   keymaps = { -- vim-surround style keymaps
     -- insert = "ys",
@@ -14,50 +12,7 @@ local default = {
     delete = "ds",
     change = "cx",
   },
-  delimiters = {
-    pairs = {
-      ["("] = { "( ", " )" },
-      [")"] = { "(", ")" },
-      ["{"] = { "{ ", " }" },
-      ["}"] = { "{", "}" },
-      ["<"] = { "< ", " >" },
-      [">"] = { "<", ">" },
-      ["["] = { "[ ", " ]" },
-      ["]"] = { "[", "]" },
-      -- Define pairs based on function evaluations!
-      ["i"] = function()
-        return {
-          utils.get_input("Enter the left delimiter: "),
-          utils.get_input("Enter the right delimiter: "),
-        }
-      end,
-      ["f"] = function()
-        return {
-          utils.get_input("Enter the function name: ") .. "(",
-          ")",
-        }
-      end,
-    },
-    separators = {
-      ["'"] = { "'", "'" },
-      ['"'] = { '"', '"' },
-      ["`"] = { "`", "`" },
-    },
-    HTML = {
-      ["t"] = "type", -- Change just the tag type
-      ["T"] = "whole", -- Change the whole tag contents
-    },
-    aliases = {
-      ["a"] = ">", -- Single character aliases apply everywhere
-      ["b"] = ")",
-      ["B"] = "}",
-      ["r"] = "]",
-      -- Table aliases only apply for changes/deletions
-      ["q"] = { '"', "'", "`" }, -- Any quote character
-      ["s"] = { ")", "]", "}", ">", "'", '"', "`" }, -- Any surrounding delimiter
-    },
-  },
-  highlight_motion = { -- Highlight before inserting/changing surrounds
+  highlight = { -- Highlight before inserting/changing surrounds
     duration = 0,
   },
 }
