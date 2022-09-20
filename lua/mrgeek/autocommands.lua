39977d829366e3769b35ae5d6799f695eeb82b10
Omar Ahmed <omarking05@gmail.com>
2022-09-20 23:23:19 +0200

    feat(autocommands): detect ansible files

--- lua/mrgeek/autocommands.lua
+++ lua/mrgeek/autocommands.lua
@@ -10,3 +10,12 @@ autocmd("TextYankPost", {
 })
 -- Format go files on save
 autocmd("BufWritePre", { pattern = "*.go", command = ":silent! lua require('go.format').goimport()" })
+-- Detect ansible files
+autocmd("BufRead", {
+  pattern = { "*.yaml", "*.yml" },
+  callback = function()
+    if vim.fn.search("hosts:\\|tasks:", "nw") then
+      vim.api.nvim_command("set ft=yaml.ansible")
+    end
+  end,
+})
