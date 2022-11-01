272bfc44ff2ab569404f33f70f4cfbe9ce819b89
Omar Ahmed <me@mrg.sh>
2022-11-02 00:26:41 +0200

    fix(plugins): remove dim plugin

--- lua/mrgeek/plugins.lua
+++ lua/mrgeek/plugins.lua
@@ -472,16 +472,6 @@ return require("packer").startup({
       config = [[ require("mrgeek.plugins.trouble") ]],
     })
 
-    use({
-      "narutoxy/dim.lua",
-      requires = { "nvim-treesitter/nvim-treesitter", "neovim/nvim-lspconfig" },
-      after = {
-        "nvim-treesitter",
-        "nvim-lspconfig",
-      },
-      config = [[ require("dim").setup({}) ]],
-    })
-
     -- CMP and snippets plugins --
     use({
       "hrsh7th/nvim-cmp",
